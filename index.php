b64f77ee293bb7c8d541a86dbf43fe6c5e3d497d
Kacper <kacper3mrowiec@gmail.com>
2024-11-20 19:05:53 +0100

    dodanie htmla i css

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+require_once 'src/controllers/AppController.php';
 
-echo 'Hi there 👋';
+$path = $_SERVER['REQUEST_URI'];
+
+$route = parse_url($path, PHP_URL_PATH);
+$route = trim($route, '/'); 
+
+//var_dump($route);
+
+$controller = new AppController();
+
+switch ($route) {
+
+    case 'index':
+        $controller->render('index');
+        break;
+
+    default:
+        echo 'Page not found!';
+        break;
+}
